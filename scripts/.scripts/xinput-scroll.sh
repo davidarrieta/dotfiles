008600da6612f5e7a836105c0fa5745901e9ad56
Claudio D. Arrieta <ArrietaDavid2001@protonmail.com>
2018-01-31 14:28:46 -0400

    Added some scripts

---
+++ scripts/.scripts/xinput-scroll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Scroll Method Enabled" 0 1 0
+
+xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 0
+
+
