31da24d646a9a5a267c514ab5d313a0b3114c293
Claudio D. Arrieta <ArrietaDavid2001@protonmail.com>
2018-02-03 19:24:56 -0400

    Created .screenlayout folder
    Now i can quickly switch between screens

---
+++ .screenlayout/laptop-screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output LVDS1 --primary --mode 1024x600 --pos 0x0 --rotate normal --output VGA1 --off
---
+++ .screenlayout/vga.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output LVDS1 --off --output VGA1 --primary --mode 1360x768 --pos 0x0 --rotate normal
